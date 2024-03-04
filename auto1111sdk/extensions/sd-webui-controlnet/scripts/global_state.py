380768f92a6c32cb43370e581187340ed9c64b08
Aditya Chebrolu <adityarchebrolu@gmail.com>
2024-03-03 22:10:27 -0600

    change controlnet models path

--- auto1111sdk/extensions/sd-webui-controlnet/scripts/global_state.py
+++ auto1111sdk/extensions/sd-webui-controlnet/scripts/global_state.py
@@ -16,7 +16,7 @@ from typing import Dict, Callable, Optional, Tuple, List
 CN_MODEL_EXTS = [".pt", ".pth", ".ckpt", ".safetensors", ".bin"]
 cn_models_dir = os.path.join(models_path, "ControlNet")
 # cn_models_dir_old = os.path.join(scripts.basedir(), "models")
-cn_models_dir_old = os.path.dirname(scripts.basedir())
+cn_models_dir_old = os.path.dirname(os.path.dirname(scripts.basedir()))
 cn_models = OrderedDict()      # "My_Lora(abcd1234)" -> C:/path/to/model.safetensors
 cn_models_names = {}  # "my_lora" -> "My_Lora(abcd1234)"
 
